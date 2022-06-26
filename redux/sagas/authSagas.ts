ad3f07640b907fd9cfd964f857a4bcc2901e82e5
sonangrai <shonahangrae@gmail.com>
2022-06-26 07:11:27 +0545

    added signup sagas

--- redux/sagas/authSagas.ts
+++ redux/sagas/authSagas.ts
@@ -1,7 +1,15 @@
 import { call, takeLatest, all, put } from "redux-saga/effects";
-import { login, loginError, loginSuccess } from "redux/actions/auth";
-import { loginApi } from "redux/query";
+import {
+  login,
+  loginError,
+  loginSuccess,
+  signup,
+  signupError,
+  signupSuccess,
+} from "redux/actions/auth";
+import { loginApi, signupApi } from "redux/query";
 
+//Login saga handler
 function* loginSaga(action) {
   try {
     const res = yield call(loginApi, action.payload);
@@ -16,6 +24,21 @@ function* loginSaga(action) {
   }
 }
 
+//Signup saga handler
+function* signupSaga(action) {
+  try {
+    const res = yield call(signupApi, action.payload);
+    //Throwing if error
+    if (res.data.hasOwnProperty("errors")) {
+      throw res.data.errors;
+    } else {
+      yield put(signupSuccess(res));
+    }
+  } catch (error) {
+    yield put(signupError(error));
+  }
+}
+
 export default function* authSagas() {
-  yield all([takeLatest(login, loginSaga)]);
+  yield all([takeLatest(login, loginSaga), takeLatest(signup, signupSaga)]);
 }
