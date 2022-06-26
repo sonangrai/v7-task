9de2c9e005f35070a707249f2b615269de842bd0
sonangrai <shonahangrae@gmail.com>
2022-06-26 07:09:21 +0545

    added signup api fun

--- redux/query/index.ts
+++ redux/query/index.ts
@@ -27,3 +27,30 @@ export const loginApi = async (data) => {
   );
   return res;
 };
+
+//Signup Api of GQL
+export const signupApi = async (data) => {
+  const res = await axios.post(
+    apiUrl,
+    {
+      query: `mutation register($username: String!,$password: String!,$confirmPassword: String!, $email: String!) {
+                    register(registerInput :{username: $username, password: $password. confirmPassword: $confirmPassword email: $email}){
+                      username,
+                      email
+                    }
+                  }`,
+      variables: {
+        username: data.username,
+        password: data.password,
+        confirmPassword: data.password,
+        email: data.email,
+      },
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return res;
+};
