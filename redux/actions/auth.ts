7117fefa0a7c4cff8a6c82112a3ac6dd3eb3843e
sonangrai <shonahangrae@gmail.com>
2022-06-26 07:04:08 +0545

    added signup actions

--- redux/actions/auth.ts
+++ redux/actions/auth.ts
@@ -1,9 +1,17 @@
 import { createAction } from "@reduxjs/toolkit";
 
+//For logging in
 export const login = createAction("LOGIN/REQ");
 export const loginSuccess = createAction<object>("LOGIN_SUCCESS/RES");
 export const loginError = createAction<object>("LOGIN_FAIL/RES");
 
+//To load user data from local
 export const loadUser = createAction<object>("LOAD_USER");
 
+//To logout user
 export const logout = createAction("LOGOUT");
+
+//FOr sign up
+export const signup = createAction("SIGNUP/REQ");
+export const signupSuccess = createAction<object>("SIGNUP_SUCCESS/RES");
+export const signupError = createAction<object>("SIGNUP_FAILED/RES");
